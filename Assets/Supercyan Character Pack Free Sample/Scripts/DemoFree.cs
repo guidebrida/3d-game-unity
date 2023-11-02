31743913c9f753acdc5af71c2f80a5ae6fcd4818
guilh <guilhermedbdb@gmail.com>
2023-11-02 12:26:26 -0300

    Player phisics and colliders

---
+++ Assets/Supercyan Character Pack Free Sample/Scripts/DemoFree.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DemoFree : MonoBehaviour
+{
+
+    private readonly string[] m_animations = { "Pickup", "Wave" };
+    private Animator[] m_animators = null;
+    [SerializeField] private FreeCameraLogic m_cameraLogic = null;
+
+    private void Start()
+    {
+        m_animators = FindObjectsOfType<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            m_cameraLogic.PreviousTarget();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            m_cameraLogic.NextTarget();
+        }
+    }
+
+
+}
